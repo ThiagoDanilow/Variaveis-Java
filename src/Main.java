1b54234d3df3a36fed9a797bf53d41221927b16e
Thiago Danilow <danilowthiago@gmail.com>
2025-04-23 11:57:43 -0300

    Variaveis.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+// Variaveis Java
+// String - stores text, such as "Hello". String values are surrounded by double quotes
+// int - stores integers (whole numbers), without decimals, such as 123 or -123
+// float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+// char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+// boolean - stores values with two states: true or false
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main{
+    public static void main(String[] args){
+
+        String name = "John";
+        int meuNumero = 10;
+        float meuFloat = 10.0f;
+        char meuChar = 'F';
+        boolean meuBoleano = true;
+
+        System.out.println(name);
+        System.out.println(meuNumero);
+        System.out.println(meuFloat);
+        System.out.println(meuChar);
+        System.out.println(meuBoleano);
+
     }
 }
