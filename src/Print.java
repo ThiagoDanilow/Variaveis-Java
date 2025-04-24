d593591ee4a2db62630ede2b99ab50ff31625731
Thiago Danilow <danilowthiago@gmail.com>
2025-04-24 10:10:14 -0300

    Testando.

---
+++ src/Print.java
@@ -0,0 +1,15 @@
+ public class Print{
+    public static void main(String[] args){
+
+        String name = "John";
+        String lastName = " Doe";
+        String fullName = name + lastName;
+
+        int x = 10;
+        int y = 20;
+
+        System.out.println("His name is " + fullName);
+        System.out.println(x + y);
+
+    }
+ }
